a10e8b57ded6028d3d9ec1412b4484b067914afe
Victor Alvarez <victor.alvarez1218@gmail.com>
2022-03-27 16:55:38 -0400

    Added Matrix class

--- strassen.cpp
+++ strassen.cpp
@@ -3,15 +3,28 @@
 
 using namespace std;
 
-// regular matrix multiplication algorithm, used after we hit base case
-vector<vector<int>> MatMult(vector<vector<int>> Mat) {
+struct Matrix {
+    int dimension;
+    Matrix(int d) {
+        dimension = d;
+    }
+};
 
+// regular matrix multiplication algorithm, used after we hit base case
+vector<vector<int>> NaiveMatMult(vector<vector<int>> M1, vector<vector<int>> M2) {
+    return M1;
+}
+
+// This helper function performs the matrix multiplications for Strassen's Algorithm
+vector<vector<int>> StrassMult(vector<vector<int>> M1, vector<vector<int>> M2) {
+    return M1;
 }
 
 // Strassen's matrix multiplication algorithm
-vector<vector<int>> Strassen(vector<vector<int>> M) {
-    return M;
+vector<vector<int>> Strassen(vector<vector<int>> M1, vector<vector<int>> M2) {
+    return M1;
 }
+
 
 int main(void) {
     return 0;
