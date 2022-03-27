b9450f97f2ed72361a09734dca6d874522bfdd33
Victor Alvarez <victor.alvarez1218@gmail.com>
2022-03-27 17:30:21 -0400

    Added naive multiplication and changed Matrix class

--- strassen.cpp
+++ strassen.cpp
@@ -4,24 +4,73 @@
 using namespace std;
 
 struct Matrix {
-    int dimension;
-    Matrix(int d) {
-        dimension = d;
+    // how many rows in matrix (height)
+    int rowDim;
+    // how many columns in matrix (width)
+    int colDim;
+    vector<vector<int>> Mat;
+
+    // constructor function
+    Matrix(int r, int c) {
+         rowDim = r;
+         colDim = c;
+        Mat.resize(r);
+        for (vector<int> row : Mat) {
+            row.resize(c);
+        }
+    }
+
+    vector<int> &operator[] (const int i) {return Mat[i];}
+
+    // method to print matrix
+    void printMat() {
+        for (vector<int> row : Mat) {
+            for (int entry : row) {
+                cout << entry  << "  " << endl;
+            }
+            cout << endl;
+        }
+    }
+
+    // add zero row to end
+    void padRow() {
+        vector<int> zeros(colDim, 0);
+        Mat.push_back(zeros);
+    }
+    
+    // add zero column to end
+    void padCol() {
+        // for each row, add a zero to the end
+        for (int i = 0; i < rowDim; i++) {
+            Mat[i].push_back(0);
+        }
     }
 };
 
 // regular matrix multiplication algorithm, used after we hit base case
-vector<vector<int>> NaiveMatMult(vector<vector<int>> M1, vector<vector<int>> M2) {
-    return M1;
+//      assumes input matrices have appropriate dimensions; (n x m) and (m x n)
+Matrix NaiveMatMult(Matrix M1, Matrix M2) {
+    Matrix toRet(M1.rowDim, M1.colDim);
+
+    for (int i = 0; i < M1.rowDim; i++) {
+        for (int j = 0; j < M1.colDim; j++) {
+            toRet[i][j] = 0;
+            for (int k = 0; k < M1.colDim; k++) {
+                toRet[i][j] += M1[i][k]*M2[k][j];
+            }
+        }
+    }
+
+    return toRet;
 }
 
 // This helper function performs the matrix multiplications for Strassen's Algorithm
-vector<vector<int>> StrassMult(vector<vector<int>> M1, vector<vector<int>> M2) {
+Matrix StrassMult(Matrix M1, Matrix M2) {
     return M1;
 }
 
 // Strassen's matrix multiplication algorithm
-vector<vector<int>> Strassen(vector<vector<int>> M1, vector<vector<int>> M2) {
+Matrix Strassen(Matrix M1, Matrix M2) {
     return M1;
 }
 
